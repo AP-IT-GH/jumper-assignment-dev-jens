d95d3c79738b4eb4e7c12df2126513d0a53e8908
Jens Baeten <Baetenjens07@gmail.com>
2022-03-29 10:49:28 +0200

    Jump Work !

--- AI-Jumper-UNITY/Assets/Scripts/JumperAgent.cs
+++ AI-Jumper-UNITY/Assets/Scripts/JumperAgent.cs
@@ -5,13 +5,24 @@ using Unity.MLAgents;
 using UnityEngine;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
+using UnityEngine.VFX;
 
 public class JumperAgent : Agent
 {
-    public float jumpForce = 10f;
+    public float jumpForce = 100f;
     
-    private Rigidbody rb = null;
-    private bool hasJumped = false;
+    private Rigidbody rb;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+
+    private bool isGrounded
+    {
+        get
+        {
+            return Physics.CheckSphere(groundCheck.position, 0.1f, groundLayer);
+        }
+        
+    }
 
     public override void Initialize()
     {
@@ -31,7 +42,12 @@ public class JumperAgent : Agent
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
         var actions = actionBuffers.DiscreteActions;
-        rb.AddForce(0, actions[0] * jumpForce, 0);
+        if (actions[0] == 1 && isGrounded)
+        {
+            rb.AddForce(0, jumpForce * 100, 0);
+            print("jumpoing actiop");
+
+        }
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -39,10 +55,9 @@ public class JumperAgent : Agent
         var discreteActionsOut = actionsOut.DiscreteActions;
         discreteActionsOut[0] = 0;
         
-        if (Input.GetKey(KeyCode.Space) && !hasJumped) {
+        if (Input.GetKey(KeyCode.Space)) {
             discreteActionsOut[0] = 1;
-            hasJumped = true;
+            print("press jump");
         }
     }
- 
 }
