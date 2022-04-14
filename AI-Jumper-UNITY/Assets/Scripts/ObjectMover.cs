72e3810f242bee3ccfaef9525e210fb68ec9bcc4
Jef <jef.v.d.a@live.be>
2022-04-14 13:57:59 +0200

    Updated object speed

--- AI-Jumper-UNITY/Assets/Scripts/ObjectMover.cs
+++ AI-Jumper-UNITY/Assets/Scripts/ObjectMover.cs
@@ -1,13 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectMover : MonoBehaviour
 {
-    public float objectSpeed = 0.1f;
+    [SerializeField]
+    private float objectSpeed = 0.1f;
+
+    private float speedMultiplier = 0.1f;
+
     void Update()
     {
-        transform.position = new Vector3(transform.position.x,transform.position.y,transform.position.z - objectSpeed);
+        transform.position = new Vector3(transform.position.x,transform.position.y,transform.position.z - (objectSpeed * speedMultiplier));
 
         if (transform.position.z <= -10)
         {
